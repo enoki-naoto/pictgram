a82752a0dd4b99670ccf90e5ea0806ca4e33f193
”名前” <>
2019-10-20 08:16:15 +0000

    create project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
-validates :name, presence: true
-validates :email,presence: true
+    validates :name, presence: true, length: {maximum: 15}
+    validates :email, presence: true,format: {with: /[.\s]*@[.\s]*.[.\s]*/}
+    validates :pass, length: {in: 8..32}, format: {with: /[a-zA-Z]*/},format: {with: /[0-9]*/}
+    validates :password_confirmation, length: {in: 8..32}, format: {with: /[a-zA-Z]*/},format: {with: /[0-9]*/}
+
+    has_secure_password
+    
 end
